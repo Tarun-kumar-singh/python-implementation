6b9d0f681f1da4670aef8883b54da249c3944271
Tarun kumar singh <tarunks2512@gmail.com>
2019-12-03 13:03:53 +0530

    binry tree creation

--- binary_tree.py
+++ binary_tree.py
@@ -12,10 +12,40 @@ class BinaryTree:
 
 	def create_tree(self):
 		data = int(input('enter root node'))
-		self.root = Node(data)
-		t = self.root
-		self
+		if data == -1:
+			return
+		new_node = Node(data)
+		self.root = new_node
+		self.create_tree2(new_node)
+		self.create_right(new_node)
 
+	def	create_tree2(self,node):
+		data = int(input('Enter the left child of {0}'.format(node.data)))
+		if(data == -1):
+			return
+		new_node = Node(data)
+		self.create_tree2(new_node)
+		node.left = new_node
+		self.create_right(new_node)
+		return
+
+	def create_right(self,node):
+		data = int(input('enter the right child of {0}'.format(node.data)))
+		if data == -1:
+			return
+		new_node = Node(data)
+		self.create_right(new_node)
+		node.right = new_node
+		self.create_tree2(new_node)
+		return
+
+	def	inorder_traversal(self):
+
+	
+
+
+b = BinaryTree()
+b.create_tree()
 
 
 		
